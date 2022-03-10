358d5eced2b1ae14b76ddb67dde770933ff0c5a1
Leo Li <Lizihaomail@gmail.com>
2022-03-10 13:03:07 -0800

    guess_num_final

--- guess_num.py
+++ guess_num.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('please enter start number')
+end = input('please enter end number')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
