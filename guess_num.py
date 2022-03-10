1a4c52a141a4be8729032e14cf215ef4065dffbf
Leo Li <Lizihaomail@gmail.com>
2022-03-10 12:55:07 -0800

    updated add count

--- guess_num.py
+++ guess_num.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = input('please guess number:')
 	num = int(num)
 	if num == r:
 		print('that is correct')
+		print('this is your', count, 'guess')
 		break
 	elif num > r:
 		print('guess lower number')
 	elif num < r:
 		print('guess higher number')
+	print('this is your', count, 'guess')
